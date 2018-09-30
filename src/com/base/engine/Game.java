5137f6276010698c236ed0afd5281f76ee4136ca
jaymaxz <jagathmax2@gamil.com>
2018-09-30 14:58:12 +0530

    video 12

--- src/com/base/engine/Game.java
+++ src/com/base/engine/Game.java
@@ -3,6 +3,7 @@ public class Game
 {
 	private Mesh mesh;
 	private Shader shader;
+	private Transform transform;
 	
 	public Game()
 	{
@@ -15,11 +16,13 @@ public class Game
 		
 		mesh.addVertices(data);
 		
+		transform = new Transform();
+		
 		shader.addVertexShader(ResourceLoader.loadShader("basicVertex.vs"));
 		shader.addFragmentShader(ResourceLoader.loadShader("basicFragment.fs"));
 		shader.compileShader();
 		
-		shader.addUniform("uniformFloat");
+		shader.addUniform("transform");
 	}
 	
 	public void input()
@@ -39,13 +42,15 @@ public class Game
 	
 	public void update()
 	{
-		temp += Time.getDelta() * 5;
-		shader.setUniformf("uniformFloat", (float)Math.abs(Math.sin(temp)));
+		temp += Time.getDelta();
+		
+		transform.setTranslation((float)Math.sin(temp), 0, 0);
 	}
 	
 	public void render()
 	{
 		shader.bind();
+		shader.setUniform("transform", transform.getTransformation());
 		mesh.draw();
 	}
 }
